68e330e58e0245cf02c28759e638a6e903b30413
Carlo de Wolf <cdewolf@redhat.com>
2018-05-22 16:09:09 +0200

    Remove junit-4.12.jar

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ant -Dcom.mysql.jdbc.jdk5=/usr -Dcom.mysql.jdbc.jdk5.javac=javac -Dcom.mysql.jdbc.jdk8.javac=javac -Dcom.mysql.jdbc.jdk8=/usr -Dwithout-jdk5=true -Dwithout-jdk8=true -Dwithout-hibernate4=true
+ant -Dcom.mysql.jdbc.extra.libs=/usr/share/java -Dcom.mysql.jdbc.jdk5=/usr -Dcom.mysql.jdbc.jdk5.javac=javac -Dcom.mysql.jdbc.jdk8.javac=javac -Dcom.mysql.jdbc.jdk8=/usr -Dwithout-jdk5=true -Dwithout-jdk8=true -Dwithout-hibernate4=true $*
